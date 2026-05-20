953ac7e6a6667760440273d41f4e51ca965509e3
Tane van Wifferen <tane.van.wifferen@proxy.nl>
2026-05-20 13:20:13 +0200

    chore: re-export side-by-side, image, markdown viewers from diff-viewers index

--- apps/desktop/src/diff-viewers/index.ts
+++ apps/desktop/src/diff-viewers/index.ts
@@ -1,3 +1,6 @@
 export type { DiffViewer, DiffViewerProps, DiffViewerCapabilities, ChangedFileEntry } from "./types";
 export { registerDiffViewer, getDiffViewer, getDefaultDiffViewer, listDiffViewers } from "./registry";
 export { inlineDiffViewer } from "./inline";
+export { sideBySideDiffViewer } from "./side-by-side";
+export { imageDiffViewer } from "./image";
+export { markdownDiffViewer } from "./markdown";
